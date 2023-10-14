410dedc659bf254221332de3037ed115b435a796
aeimo <147949549+aeimo@users.noreply.github.com>
2023-10-14 16:45:09 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Privacy and Security Checker</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <div class="popup-content">
+    <h2>Privacy Score:</h2>
+    <div id="privacy-score"></div>
+    
+    <h2>Security Concerns:</h2>
+    <ul id="security-concerns">
+      <li>No HTTPS</li>
+      <li>Outdated SSL certificate</li>
+    </ul>
+  </div>
+  <script src="popup.js"></script>
+</body>
+</html>
